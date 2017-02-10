0fa7f4c20401eb641b7c90cca3831394f7ddeda1
Julius von Froreich <fvj@users.noreply.github.com>
2017-02-10 18:37:20 +0100

    delete multipart due to implementation complexity; convert to sane coding style

--- source/index.js
+++ source/index.js
@@ -2,65 +2,54 @@
  * @description Body-Parsing Middleware
  * This is a curried function with options, so you have to
  * call it once before passing it to spirit
- * @example const body = spiritBody({json: true, urlEncoded: true, multiPart: true})
+ * @param {Object} options The parsing components to be used.
+ * @return {function} Middleware to be used.
+ * @example const body = spiritBody({json: true, urlEncoded: true})
  */
 export default (options = {
-	multiPart: false,
-	json: false,
-	urlEncoded: false
+  multiPart: false,
+  json: false,
 }) => handler => request => new Promise((resolve, reject) => {
-	const req = request.req()
+  const req = request.req();
 
-	// Push to array for (most likely) improved concatenation performance
-	const chunks = []
-	req.on('data', data =>
-		chunks.push(
-			data.toString()
-		)
-	)
+  // Push to array for (most likely) improved concatenation performance
+  const chunks = [];
+  req.on('data', data => chunks.push(data));
 
-	// connection ended
-	req.on('end', () => {
-		const raw = chunks.join('')
+  // connection ended
+  req.on('end', () => {
+    // concat the chunks and convert to String---that's ok, because we are only
+    // being sent plaintext, Buffer defaults to utf8. TODO: check for other enc.
+    const raw = Buffer.concat(chunks).toString();
 
-		// lowercase headers
-		const lowercaseHeaders = Object
-			.keys(request.headers)
-			.map(key => ({
-				[key.toLowerCase()]: request.headers[key]
-			}))
-			.reduce((previous, current) => Object.assign(previous, current), {})
+    // convert the headers to lowercase for easier comparison
+    const lowercaseHeaders = Object
+      .keys(request.headers)
+      .map(key => ({
+        [key.toLowerCase()]: request.headers[key],
+      }))
+      .reduce((previous, current) => Object.assign(previous, current), {});
 
-		// parse
-		const [type] = (lowercaseHeaders['content-type'] || '').split(';')
+    const [type] = (lowercaseHeaders['content-type'] || '').split(';');
 
-		if (options.urlEncoded && type === 'application/x-www-form-urlencoded') {
+    if (options.urlEncoded && type === 'application/x-www-form-urlencoded') {
+      const result = {};
+      const keyValueRegex = /([^&=]+)=?([^&]*)/g;
+      const sanitize = input => decodeURIComponent(input.replace(/[+]/g, ' '));
 
-			const result = {}
-			const keyValueRegex = /([^&=]+)=?([^&]*)/g
-			const sanitize = input => decodeURIComponent(input.replace(/[+]/g, ' '))
+      let match;
+      while (match = keyValueRegex.exec(raw)) {
+        const [, key, value] = match.map(sanitize);
+        result[key] = value;
+      }
+      request.body = result;
+    } else if (options.json &&
+        ['application/json', 'text/json'].includes(type)) {
+      request.body = JSON.parse(raw);
+    } else {
+      request.body = raw;
+    }
 
-			// parse key-value pairs
-			let match
-			while (match = keyValueRegex.exec(raw)) {
-				const [,key, value] = match.map(sanitize)
-				result[key] = value
-			}
-			// set body
-			request.body = result
-
-		} else if (options.json && ['application/json', 'text/json'].includes(type)) {
-
-			request.body = JSON.parse(raw)
-
-		} else if (!'TODO' && options.multiPart && type === 'multipart/form-data') {
-
-			// TODO
-
-		} else {
-			request.body = raw
-		}
-
-		resolve(handler(request))
-	});
-})
+    resolve(handler(request));
+  });
+});
