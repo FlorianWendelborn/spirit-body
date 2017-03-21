ed187828c5d9524804e26ce6f72978aee8afa882
Florian Wendelborn <dodekeract@users.noreply.github.com>
2017-03-21 14:45:54 +0100

    Fix Whitespace

--- source/index.js
+++ source/index.js
@@ -7,49 +7,49 @@
  * @example const body = spiritBody({json: true, urlEncoded: true})
  */
 export default (options = {
-  multiPart: false,
-  json: false,
+	multiPart: false,
+	json: false,
 }) => handler => request => new Promise((resolve, reject) => {
-  const req = request.req();
+	const req = request.req();
 
-  // Push to array for (most likely) improved concatenation performance
-  const chunks = [];
-  req.on('data', data => chunks.push(data));
+	// Push to array for (most likely) improved concatenation performance
+	const chunks = [];
+	req.on('data', data => chunks.push(data));
 
-  // connection ended
-  req.on('end', () => {
-    // concat the chunks and convert to String---that's ok, because we are only
-    // being sent plaintext, Buffer defaults to utf8. TODO: check for other enc.
-    const raw = Buffer.concat(chunks).toString();
+	// connection ended
+	req.on('end', () => {
+		// concat the chunks and convert to String---that's ok, because we are only
+		// being sent plaintext, Buffer defaults to utf8. TODO: check for other enc.
+		const raw = Buffer.concat(chunks).toString();
 
-    // convert the headers to lowercase for easier comparison
-    const lowercaseHeaders = Object
-      .keys(request.headers)
-      .map(key => ({
-        [key.toLowerCase()]: request.headers[key],
-      }))
-      .reduce((previous, current) => Object.assign(previous, current), {});
+		// convert the headers to lowercase for easier comparison
+		const lowercaseHeaders = Object
+			.keys(request.headers)
+			.map(key => ({
+				[key.toLowerCase()]: request.headers[key],
+			}))
+			.reduce((previous, current) => Object.assign(previous, current), {});
 
-    const [type] = (lowercaseHeaders['content-type'] || '').split(';');
+		const [type] = (lowercaseHeaders['content-type'] || '').split(';');
 
-    if (options.urlEncoded && type === 'application/x-www-form-urlencoded') {
-      const result = {};
-      const keyValueRegex = /([^&=]+)=?([^&]*)/g;
-      const sanitize = input => decodeURIComponent(input.replace(/[+]/g, ' '));
+		if (options.urlEncoded && type === 'application/x-www-form-urlencoded') {
+			const result = {};
+			const keyValueRegex = /([^&=]+)=?([^&]*)/g;
+			const sanitize = input => decodeURIComponent(input.replace(/[+]/g, ' '));
 
-      let match;
-      while (match = keyValueRegex.exec(raw)) {
-        const [, key, value] = match.map(sanitize);
-        result[key] = value;
-      }
-      request.body = result;
-    } else if (options.json &&
-        ['application/json', 'text/json'].includes(type)) {
-      request.body = JSON.parse(raw);
-    } else {
-      request.body = raw;
-    }
+			let match;
+			while (match = keyValueRegex.exec(raw)) {
+				const [, key, value] = match.map(sanitize);
+				result[key] = value;
+			}
+			request.body = result;
+		} else if (options.json &&
+				['application/json', 'text/json'].includes(type)) {
+			request.body = JSON.parse(raw);
+		} else {
+			request.body = raw;
+		}
 
-    resolve(handler(request));
-  });
+		resolve(handler(request));
+	});
 });
